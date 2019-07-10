f3e59b5c061223a318e6a51e24ec51f1260a755e
Austin Raney <aaraney@crimson.ua.edu>
2019-07-09 22:19:31 -0500

    Domain setup framework alpha 0.1

--- framework_dev/JobScheduler/NwmDomainSetup.py
+++ framework_dev/JobScheduler/NwmDomainSetup.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 
-from os import mkdir, symlink
-from os.path import join, realpath, basename, exists
+from os import link, makedirs, remove
+from os.path import join, realpath, basename
 from glob import glob
 
 
@@ -15,36 +15,54 @@ class NwmDomainSetup:
 
         self.setupModel()
 
-    def __symlink(self, src_path, dest_path):
+    def cleanUp(self):
+        '''
+        Remove forcings and restarts
+        Aggregate files to better location
+        '''
+    pass
+
+    def __link(self, dest_dir, *src_files):
         '''
         Create a symlink. This was added to ensure that
         full pathnames were used when linking files --
         as mpi does not handle relative links well.
         '''
-        # May need to add handler for existing links
-        if type(src_path) == list:
-            # Create symlinks for list of src_paths to dest_path
-            for src_item in src_path:
-                src_item_path = realpath(src_item)
-                des_item_path = join(dest_path, basename(src_item_path))
-                symlink(src_item_path, des_item_path)
-        else:
-            symlink(realpath(src_path), join(dest_path, basename(src_path)))
+        # link full path of src file to dest_dir using
+        # the name of src file
+        def _link(src):
+            link(realpath(src), join(dest_dir, basename(src)))
+        try:
+            if src_files:
+                for item in src_files:
+                    _link(item)
+        except FileExistsError:
+            # handle existing links
+            for item in src_files:
+                remove(join(dest_dir, basename(item)))
+            self.__link(dest_dir, *src_files)
 
-    def replaceDomainFiles(self, alt_domain_list):
+    def populateDomainFiles(self, alt_domain_list):
         # This could be recoded in the future to be more
         # readable/better coded.
-        if not len(alt_domain_list):
-            return []
 
+        # set of master domain files
         master_domain_files = set(glob(join(self.master_dir, 'DOMAIN/*.nc')))
+
+        # helper function for finding matching domain files
+        # this should be coded differently
         join_master_dir = lambda x: join(self.master_dir, 'DOMAIN/{}'.format(basename(x)))
 
-        for item in alt_domain_list:
-            if join_master_dir(item) in master_domain_files:
-                master_domain_files.remove(join_master_dir(item))
-                master_domain_files.add(item)
-        return list(master_domain_files)
+        try:
+            for item in alt_domain_list:
+                if join_master_dir(item) in master_domain_files:
+                    master_domain_files.remove(join_master_dir(item))
+                    master_domain_files.add(item)
+            return list(master_domain_files)
+
+        except:
+            # if no alt domain files, then just return master
+            return list(master_domain_files)
 
     def setupModel(self):
         '''
@@ -57,29 +75,33 @@ class NwmDomainSetup:
         for writing meta_data
         '''
 
-        # if slave path does not exist, create it
-        if not exists(self.slave_dir):
-            mkdir(self.slave_dir)
+        # Create dirs
+        makedirs(self.slave_dir, exist_ok=True)
+        makedirs(join(self.slave_dir, 'DOMAIN'), exist_ok=True)
+        makedirs(join(self.slave_dir, 'FORCING'), exist_ok=True)
+
 
         # Symlink TBL and wrf_hydro.exe to slave
         # THIS NEEDS TO BE CHANGED TO INTERACT WITH CONTAINER
-        self.__symlink(glob(join(self.nwm_src, '*.TBL')), self.slave_dir)
-        self.__symlink(glob(join(self.nwm_src, 'wrf_hydro.exe')), self.slave_dir)
+        # self.__link(self.slave_dir, *glob(join(self.nwm_src, '*.TBL')))
+        # self.__link(self.slave_dir, 'wrf_hydro.exe')
 
         # mkdir and symlink DOMAIN and FORCING files
-        domain_file_list = self.replaceDomainFiles(self.alt_domain_list)
+        domain_file_list = self.populateDomainFiles(self.alt_domain_list)
 
-        mkdir(join(self.slave_dir, 'DOMAIN'))
-        mkdir(join(self.slave_dir, 'FORCING'))
+        self.__link(join(self.slave_dir, 'DOMAIN'), *domain_file_list)
+        self.__link(join(self.slave_dir, 'FORCING'), *glob(join(self.master_dir, 'FORCING/*')))
 
-        self.__symlink(domain_file_list, join(self.slave_dir, 'DOMAIN'))
-        self.__symlink(join(self.master_dir, 'FORCING'), join(self.slave_dir, 'FORCING'))
+        # link namelist files
+        self.__link(self.slave_dir, *glob(join(self.master_dir, '*namelist*')))
+        # self.__link(domain_file_list, join(self.slave_dir, 'DOMAIN'))
+        # self.__link(join(self.master_dir, 'FORCING'), join(self.slave_dir, 'FORCING'))
 
 
 if __name__=='__main__':
-    master_path = '/Users/austinraney/Box Sync/si/local/framework_dev/SIPSEY_WILDERNESS_DOMAIN'
-    slave_path = '/Users/austinraney/Box Sync/si/local/framework_dev/test1'
+    master_path = '/Users/austinraney/Box Sync/si/sandbox/framework_test/master_domain'
+    slave_path = '/Users/austinraney/Box Sync/si/sandbox/framework_test/slave_domain'
     nwm_path = '/Users/austinraney/Box Sync/si/local/ms_domain1'
-    domain_files = ['/Users/austinraney/Box Sync/si/nwm/domains/cahaba_nwm_subset/f495f0293071225d21af98fa455e148662c442b8/geo_em.d01.nc', '/Users/austinraney/Box Sync/si/nwm/domains/cahaba_nwm_subset/f495f0293071225d21af98fa455e148662c442b8/Route_Link.nc']
+    domain_files = ['/Users/austinraney/Box Sync/si/sandbox/docker_testing/framework_test/master_domain/DOMAIN/geo_em.d01.nc']
 
-    nwm = NwmDomainSetup(master_path,slave_path,nwm_src=nwm_path,alt_domain_list=domain_files)
+    nwm = NwmDomainSetup(master_path, slave_path)
--- framework_dev/JobScheduler/Scheduler.py
+++ framework_dev/JobScheduler/Scheduler.py
@@ -4,14 +4,108 @@ import os
 import docker
 from JobScheduler.Job import Job
 from queue import Queue
+import glob
+from functools import reduce
+import operator
 
-# Currently only supporting local docker client
-# However, see https://docker-py.readthedocs.io/en/stable/client.html
-# to implement a remote docker server
-# docker_client = docker.from_env()
 
-jobQ = Queue()
+class Scheduler:
+
+    def __init__(self, docker_client=None):
+        if not docker_client:
+            self.docker_client = docker.from_env()
+        else:
+            self.docker_client = docker_client
+
+        self.checkHeartBeat()
+
+        self.alt_domain_file_list = []
+
+        self.runningContainerDict = {}
+        self.jobQ = Queue()
+
+        self._image_tag = None
+        # Max jobs is the max number of jobs
+        # running at any given time
+        self._MAX_JOBS = 1
+
+        # Max cps is the max number of cpus
+        # running at any given time
+        self._MAX_CPUS = 1
+
+    @property
+    def MAX_JOBS(self):
+        return self._MAX_JOBS
+    @MAX_JOBS.setter
+    def MAX_JOBS(self, max_jobs):
+        self._MAX_JOBS = max_jobs
+
+    @property
+    def MAX_CPUS(self):
+        return self._MAX_CPUS
+    @MAX_CPUS.setter
+    def MAX_CPUS(self, max_cpus):
+        self._MAX_CPUS = max_cpus
+
+    @property
+    def image_tag(self):
+        return self._image_tag
+
+    @image_tag.setter
+    def image_tag(self, tag):
+        self._image_tag = tag
+
+    @image_tag.deleter
+    def image_tag(self):
+        self._image_tag = None
+
+
+    def checkHeartBeat(self):
+        # Currently only supporting local docker client
+        # However, see https://docker-py.readthedocs.io/en/stable/client.html
+        # to implement a remote docker server
+        try:
+            docker_client = docker.from_env()
+            # Check for a heartbeat
+            docker_client.ping()
+        except ConnectionError:
+            print("Please check that the Docker Daemon is running.")
+            exit(1)
+
+    def check_for_image(self, image_tag):
+        # Check if user has specified container pulled
+        docker_image_list = self.docker_client.images.list()
+        # Create flattened list of RepoTags of images pulled on docker client
+        docker_repo_tag_list = reduce(operator.concat, list(map(lambda x: x.attrs['RepoTags'], docker_image_list)))
+
+        # Return bool if image is pulled
+        return image_tag in docker_repo_tag_list
+
+    def runJob(self, image_tag, slave, cpuset):
+        volumes = {
+            slave : {'bind': '/slave',
+                     'mode': 'rw'}
+        }
+        return self.docker_client.containers.run(image_tag, cpuset_cpus=cpuset, detach=True, remove=True, volumes=volumes)
+
+    def startJobs(self):
+        while self.jobQ.qsize() != 0:
+            container_list = self.docker_client.containers.list()
+            if len(container_list) < self.MAX_JOBS:
+                self.runJob()
+
+
+
+            self.docker_client.containers.run(self.image_tag, entrypoint='something')
+            client.containers.run('alpine', 'echo hello world')
+
+        job = Job()
+
+
+
 
 if __name__=='__main__':
-    j = Job('test','test','test','test')
-    print(j)
+    s = Scheduler()
+    slave = '/Users/austinraney/Box Sync/si/sandbox/framework_test/slave_domain'
+    print(s.runJob('aaraney/nwm-run-env', slave, '0-3'))
+
