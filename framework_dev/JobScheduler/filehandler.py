99d3a89f0580e5bb2e4657d0bdd23c5b77d494cd
Austin Raney <aaraney@crimson.ua.edu>
2019-07-11 14:17:14 -0500

    Added support for all domain file types

--- framework_dev/JobScheduler/filehandler.py
+++ framework_dev/JobScheduler/filehandler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import xarray as xr
+from os.path import basename
 
 '''
 This module is used to link
@@ -11,9 +12,21 @@ to the job scheduler
 # Dict of unique variables in
 # domain files mapped to their
 # common name convention in NWM/Wrf-Hydro
-# TODO: Add other unique keys and files names
+# Note: GEOGRID_LDASOUT_Spatial_Metadata.nc
+# is explicitly looked for based on its basename
+# because it does not have a unique variable
 file_type_dict = {
-    'link': 'Route_link.nc'
+    'link': 'Route_link.nc',
+    'basn_msk': 'Fulldom_hires.nc',
+    'ALBEDO12M': 'geo_em.d01.nc',
+    'Area_sqkm': 'GWBUCKPARM.nc',
+    'OV_ROUGH2D': 'hydro2dtbl.nc',
+    'ifd': 'LAKEPARM.nc',
+    'qThresh': 'nudgingParams.nc',
+    'bexp': 'soil_properties.nc',
+    'weight': 'spatialweights.nc',
+    'CANWAT': 'wrfinput_d01.nc'
+
 }
 
 def identifyDomainFile(domain_file):
@@ -27,11 +40,14 @@ def identifyDomainFile(domain_file):
     for key in file_type_dict:
         if key in domain_file_xr.keys():
             return file_type_dict[key]
+        # Explicitly return GEOGRID_LDASOUT_Spatial_Metadata.nc
+        elif basename(domain_file) == 'GEOGRID_LDASOUT_Spatial_Metadata.nc':
+            return basename(domain_file)
     else:
         print('Check the validity of your domain files')
         raise KeyError
 
 if __name__ == '__main__':
-    file = "/Users/austinraney/Box Sync/si/nwm/domains/pocono/Route_Link.nc"
+    file = '/Users/austinraney/Box Sync/si/nwm/domains/pocono/NWM/DOMAIN/GEOGRID_LDASOUT_Spatial_Metadata.nc'
     x = xr.open_dataset(file)
     print(identifyDomainFile(file))
