467c144083a5c9c45a4f8d7b1a0924a4b12689b9
imanmaghami <33031286+imanmaghami@users.noreply.github.com>
2019-07-11 09:36:44 -0500

    The code is now more general

--- framework_dev/perturb_py_dev/Parameter_editor.py
+++ framework_dev/perturb_py_dev/Parameter_editor.py
@@ -1,114 +1,170 @@
 import xarray as xr
 import numpy as np
 
-# rt_lnk = "C:/Users/Iman/Desktop/RouteLink_NWMv2.0_20190517_cheyenne_pull.nc"
-# org_lnk = "C:/Users/Iman/Desktop/sipsey_wilderness/Route_Link.nc"
-#
-# org_rt = xr.open_dataset(org_lnk)
-# rt = xr.open_dataset(rt_lnk)
-#
-# max_lat = max(org_rt.lat.values)
-# min_lat = min(org_rt.lat.values) - 0.0001
-# max_lon = max(org_rt.lon.values) + 0.0001
-# min_lon = min(org_rt.lon.values)
-#
-# # print(rt.variables)
-# new_rt = rt.where((rt.lat >= min_lat) & (rt.lat <= max_lat) & (rt.lon >= min_lon) & (rt.lon <= max_lon), drop=True)
-# # print(new_rt.variables)
-#
-# new_rt.to_netcdf("C:/Users/Iman/Desktop/RouteLink_NWMv2.0_20190517_cheyenne_pull_subset.nc")
-# # -87.59833,-87.26391,34.20192,34.49317
 
-
+# --------------------------------------------------------------------------------------------------
+# ---------------------------------  functions -----------------------------------------------------
+# --------------------------------------------------------------------------------------------------
 # Function to modify the input files in the netcdf file
-def para_editor(File_name, para_name , editing_mode = 'Scl', scale = 1.1):
-    '''(netcdf file path and name, parameter name to be modified, editing mode: 'Scl' for Scaler and 'StOr' for BasedoOnStreamOrder , scale)'''
+
+def cross_section_area_BlckBrn(TopWdth):
+    '''(Top Width of Trapezoidal Channel)'''
+    # This function calculates the cross section area from Topwidth using Blackburn et al. equations
+    cs_area = 0.75 * ((TopWdth / 2.44) ** (1.0 / 0.34)) ** 0.53
+
+    return cs_area
+
+
+def para_editor_evenly_scaler(ds, para_name, scale):
+    '''(dateset, parameter name to be modified, scale)'''
+    # This function scales the chosen parameter and the dependent parameters if any.
 
     valid_para_to_edit = {'ChSlp', 'n', 'nCC', 'TopWdth', 'TopWdthCC', 'BtmWdth'}
-    editing_mode_list = {'Scl', 'StOr'}
-
     if para_name not in valid_para_to_edit:
         raise ValueError("results: parameter_name to be edited must be one of %r." % valid_para_to_edit)
 
-    if editing_mode not in editing_mode_list:
-        raise ValueError("results: editing_mode must be one of %r." % editing_mode_list)
 
-    ds = xr.open_dataset(File_name) # Open the netcdf file
-    print('opened file: ', File_name)
-
-    if editing_mode == 'Scl':
-        ds[para_name][:] *= scale  # Modify the parameter
-
-        # !!!Be advised: should better define the cs_area, currently, it is a xarray dataframe and the variable name is
-        # not correct...but it works!
-        if para_name == 'ChSlp':
-            #  Following 2 equations are from Blackburn et al.
-            cs_area = 0.75*((ds['TopWdth'][:]/2.44)**(1.0 / 0.34))**0.53  # cs_area is not a varaible in NWM dataframe
-                                                                          # but is necessary to be calculated in order
-                                                                          # to be able to calculate BtmWdth to
-
-            # Be careful of neg values being root squared: happens when dramatically decrease ChSlp
-            ds['BtmWdth'][:] = (ds['TopWdth'][:]**2-(4.0*cs_area)/ds['ChSlp'][:])**0.5
+    ds[para_name][:] *= scale  # Modify the parameter
+    ds.attrs['Edits_made'] += '|| Para ' + str(para_name) + ' scaled by ' + str(scale)  # Modify the MetaData
 
 
-        if para_name == 'BtmWdth':
-            #  Following 2 equations are from Blackburn et al.
-            cs_area = 0.75 * ((ds['TopWdth'][:] / 2.44) ** (1.0 / 0.34)) ** 0.53
-            # cs_area is not a varaible in NWM dataframe
-            # but is necessary to be calculated in order
-            # to be able to calculate BtmWdth to
+    # !!!Be advised: should better define the cs_area, currently, it is a xarray dataframe and the variable name is
+    # not correct...but it works!
+    if para_name == 'ChSlp':
+        #  Following 2 equations are from Blackburn et al.
+        cs_area = cross_section_area_BlckBrn(ds['TopWdth'][:])  # cs_area is not a varaible in NWM dataframe
+        # but is necessary to be calculated in order to be able to calculate BtmWdth to
 
-            ds['ChSlp'][:] = 4.0 * cs_area / (ds['TopWdth'][:] ** 2 - ds['BtmWdth'][:] ** 2)
+        # Be careful of neg values being root squared: happens when dramatically decrease ChSlp
+        ds['BtmWdth'][:] = (ds['TopWdth'][:]**2-(4.0*cs_area)/ds['ChSlp'][:])**0.5
+
+        ds.attrs['Edits_made'] += ' ** Also, para ' + 'BtmWdth' + ' was changed as dependent para '  # Modify the MetaData
 
 
+    if para_name == 'BtmWdth':
+        #  Following 2 equations are from Blackburn et al.
+        cs_area = cross_section_area_BlckBrn(ds['TopWdth'][:])
+        ds['ChSlp'][:] = (ds['TopWdth'][:] ** 2 - ds['BtmWdth'][:] ** 2) / 4.0 * cs_area  # it got inversed since CHSlop H/V
 
-    # !!!Be advised: Currently StOr is NOT complete
-    if editing_mode == 'StOr':
-        for i in range(int(max(new_rt.order.values))):
-            ds[para_name] = xr.where(ds.order == i, ds[para_name] * scale * (i / 10 + 1), ds[para_name])
+        ds.attrs['Edits_made'] += ' ** Also, param ' + 'ChSlp' + ' was changed as dependent para '  # Modify the MetaData
 
     return ds
 
+def para_editor_evenly_scaler_multi_para(InputFilename, outputpath, para_names, scales_list):
+    '''(Input file to be edited, outputpath, list of parameters to be edited, list of scales corresponding to the parameters)'''
+    # This function scales multiple parameters and the dependent parameters if any.
+    ds = xr.open_dataset(InputFilename)  # Open the netcdf file
 
-def copier(xr_dataframe, outputpath):
-    '''(Output from function para_editor which is an edited RouteLink xarray dataset, path to copy the file)'''
+    ds1 = ds  # Copy the dataset
+    ds1.attrs['Edits_made'] = ''  # Add Global Attribute to track the changes made on parameters
 
-    xr_dataframe.to_netcdf(outputpath)
+    # edit the netcdf files
+    for i, para_name in enumerate(para_names):
+        ds1 = para_editor_evenly_scaler(ds1, para_name, scales_list[i])
+
+    # Create the outputfile name
+    for i, para_name in enumerate(para_names):
+        outputpath += '_p' + str(i) + '_' + para_name + '_sc_' + str(scales_list[i])
+    outputpath += '.nc'
+
+    # Create the edited netcdf file
+    ds1.to_netcdf(outputpath)
+
+    # #  Read th# e edited netcdf file
+    # ds1.close()
+    # del ds1
+    # ds1 = xr.open_dataset(outputpath)  # Open the netcdf file
+
+    return ds1
 
 
-#################################################################################
-###                                     Main                                  ###
-#################################################################################
+def para_editor_streamorder_based_scaler(ds, para_name, streamorder_list, scale_list):
+    '''(dateset, parameter name to be modified, scaler list for each streamorder)'''
+    # This function scales the chosen parameter and the dependent parameters if any.
 
-Para = 'BtmWdth'  # parameter to be edited
+    valid_para_to_edit = {'ChSlp', 'n', 'nCC', 'TopWdth', 'TopWdthCC', 'BtmWdth'}
+    if para_name not in valid_para_to_edit:
+        raise ValueError("results: parameter_name to be edited must be one of %r." % valid_para_to_edit)
 
-# A simple parameter perturber
-scale_list = []
-for i in np.arange(0.4, 1.66667, 0.2):  # Do not exceed 1.66667, as by keeping TopWdth constant and multiplying BtmWdth
-                                    # by 1.66667, it will get bigger than TopWdth leading to a reverse trapezoidal which
-                                    # is not what we want...
-    scale_list.append(i)
+    for i, streamorder in enumerate(streamorder_list):
+        ds[para_name] = xr.where(ds.order == streamorder, ds[para_name] * scale_list[i], ds[para_name])
 
-# Input netcdf file for study area
-FileName = "C:/Users/Iman/Desktop/RouteLink_NWMv2.0_20190517_cheyenne_pull_subset.nc"
+    ds.attrs['Edits_made'] += '|| Para ' + para_name + ' scaled by scales ' + str(scale_list) + ' for streamorders ' + str(streamorder_list)  # Modify the MetaData
 
-# Edited netcdf file
-outputpath = "C:/Users/Iman/Desktop/RouteLink_NWMv2.0_20190517_cheyenne_pull_subset_edit"
 
-# perturb parameter, edit file, copy the edited file, and repeat...
-for counter, scale in enumerate(scale_list):
-    print('\nCounter: ', counter+1, '  Scaler: ', scale)
+    # !!!Be advised: should better define the cs_area, currently, it is a xarray dataframe and the variable name is
+    # not correct...but it works!
+    if para_name == 'ChSlp':
 
-    new2_rt = para_editor(FileName, Para , editing_mode='Scl', scale=scale)
+        #  Following 2 equations are from Blackburn et al.
+        cs_area = cross_section_area_BlckBrn(ds['TopWdth'][:])
+        # Be careful of neg values being root squared: happens when dramatically decrease ChSlp
+        ds['BtmWdth'][:] = (ds['TopWdth'][:]**2-(4.0*cs_area)/ds['ChSlp'][:])**0.5 # IMPORTANT: CHECK IT OUT
+        ds.attrs['Edits_made'] += ' ** Also, para ' + 'BtmWdth' + ' was changed as dependent para '  # Modify the MetaData
 
-    # Print some of the parameters after perturb
-    print('First value for ' + 'ChSlp' + ': ', new2_rt['ChSlp'][0].values)
-    print('First value for ' + 'BtmWdth' + ': ', new2_rt['BtmWdth'][0].values)
-    print('First value for ' + 'TopWdth' + ': ', new2_rt['TopWdth'][0].values)
-    print('First value for ' + 'CrossSectionArea' + ': ', 0.75*((new2_rt['TopWdth'][0].values/2.44)**(1.0 / 0.34))**0.53)
-    print('First value for ' + 'n' + ': ', new2_rt['n'][0].values)
+    if para_name == 'BtmWdth':
+        #  Following 2 equations are from Blackburn et al.
+        cs_area = cross_section_area_BlckBrn(ds['TopWdth'][:])
+        ds['ChSlp'][:] = (ds['TopWdth'][:] ** 2 - ds['BtmWdth'][:] ** 2) / 4.0 * cs_area  # it got inversed since CHSlop H/V
+        ds.attrs['Edits_made'] += ' ** Also, param ' + 'ChSlp' + ' was changed as dependent para '  # Modify the MetaData
 
-    copier(new2_rt, outputpath + '_para_' + Para + '_scale_' + str(scale) + '.nc')
+    return ds
+
+# --------------------------------------------------------------------------------------------------
+# -------------------------------------------- Main ------------------------------------------------
+# --------------------------------------------------------------------------------------------------
+
+Filename = "C:/Users/Iman/Desktop/RouteLink_NWMv2.0_20190517_cheyenne_pull_subset_Copy.nc"
+outputpath = "C:/Users/Iman/Desktop/RouteLink_NWMv2.0_20190517_cheyenne_pull_subset"
+
+
+# --------------------------------- Example of Evenly Scaler----------------------------------------
+params_list = ['BtmWdth', 'n', 'nCC']  # parameters to be edited
+scales_list_of_lists = [[1.1, 1.2, 1.5], [1.1, 1.2, 4], [1.1, 1.2, 5], [1.1, 1.2, 6], [1.1, 1.2, 10], [1.1, 1.2, 20]]
+
+for scales_list in scales_list_of_lists:
+    ds1 = para_editor_evenly_scaler_multi_para(Filename, outputpath, params_list, scales_list)
+
+    print'\n**********************'
+    # print('First value for ' + 'ChSlp' + ': ', ds1['ChSlp'][0].values)
+    # print('First value for ' + 'BtmWdth' + ': ', ds1['BtmWdth'][0].values)
+    # print('First value for ' + 'TopWdth' + ': ', ds1['TopWdth'][0].values)
+    # print('First value for ' + 'CrossSectionArea' + ': ', 0.75 * ((ds1['TopWdth'][0].values / 2.44) ** (1.0 / 0.34)) ** 0.53)
+    # print('First value for ' + 'n' + ': ', ds1['n'][0].values)
+    print('First value for ' + 'nCC' + ': ', ds1['nCC'][0].values)
+    print ds1.Edits_made.split('||')
+
+
+# --------------------------Example of Scaler based on Streamorder----------------------------------
+para_name = 'BtmWdth'
+ds = xr.open_dataset(Filename)
+
+# Create list of streamorders present in the domain file
+Streamorder_list = []
+for i in range(min(ds.order), max(ds.order)+1):
+    Streamorder_list.append(i)
+
+# Arbitrarily, create list of scalers corrsoponding to each streamorder
+scaler_list = []
+if max(ds.order) == 5:
+    scaler_list = [0.65, 0.75, 0.90, 1.1, 1.25]
+elif max(ds.order) == 4:
+    scaler_list = [0.75, 0.9, 1.1, 1.25]
+
+ds.attrs['Edits_made'] = ''  # Add Global Attribute to track the changes made on parameters
+ds1 = para_editor_streamorder_based_scaler(ds, para_name, Streamorder_list, scaler_list)
+
+# Create the outputfile name
+for i, Streamorder in enumerate(Streamorder_list):
+    outputpath += '_p_' + para_name + '_Streamorder_' + str(Streamorder) + '_sc_' + str(scaler_list[i])
+outputpath += '.nc'
+
+# Create the edited netcdf file
+ds1.to_netcdf(outputpath)
+
+
+
+
 
 
 
