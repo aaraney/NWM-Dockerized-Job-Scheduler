d3571ca469e800e0cdd219e2485f817b24b1ee30
Austin Raney <aaraney@crimson.ua.edu>
2020-04-01 00:02:13 -0500

    Remove old code, latin hypercube library, and add a few TODO's

--- framework/perturbation_engine/parameter_editor.py
+++ framework/perturbation_engine/parameter_editor.py
@@ -1,10 +1,8 @@
+#!/usr/bin/env python3
+
 import xarray as xr
 import numpy as np
 import operator
-from SALib.sample import latin
-
-# TODO: impliment a function to create the methods_dict used by the
-# edit_parameters function
 
 # Local package imports 
 # TODO: Change to package path once refracted
@@ -211,155 +209,9 @@ def edit_parameters(fn, parameters, operators, values):
 
     return _apply_functions(df, parameter_operator_dict) 
 
-# def edit_parameter(fn, ds, para_name, scale):
-#     '''
-#     This function scales the chosen parameter and the dependent parameters if
-#     any.
-#     '''
-
-#     valid_files_to_edit = {
-#     'Route_Link.nc': {'ChSlp', 'n', 'nCC', 'TopWdth', 'TopWdthCC', 'BtmWdth'},
-#     'GWBUCKPARM.nc' : {'Expon', 'Zinit', 'Zmax'},
-#     'LAKEPARM.nc' : {'OrificeA', 'OrificeC', 'OrificeE', 'WeirC', 'WeirE', 'WeirL'},
-#     'Soil_properties.nc': {'mfsno'},
-#     'Fulldom_hires.nc' : {'LKSATFAC', 'OVROUGHRTFAC', 'RETDEPRTFAC'}
-#     }
-
-#     try:
-#         fn_w_nwm_naming_convention = filehandler.identifyDomainFile(fn)
-#         ds = xr.open_dataset(InputFilepath + '\\' + InputFilename)  # Open the netcdf file
-
-#         valid_params = valid_files_to_edit[fn]
-
-#         param_to_edit = valid_params[para_name]
-
-#     except ValueError:
-#         raise IOError('The provided parameter, {}, is not valid, please provide' \
-#                       ' a valid WRF-Hydro/NWM parameter for the input file {}'.format(para_name, valid_files_to_edit))
-
-#     # KeyError thrown by filehandler if not a valid NWM file
-#     except KeyError:
-#         raise IOError('The provided parameter file, {}, is not valid, please provide a valid WRF-Hydro/NWM parameter file'.format(fn))
-
-
-#     # Something like:
-
-#     # Need to make call to verify what kind of file it is, should be able to use function from Job Scheduler
-#     try:
-#         valid_params = valid_files_to_edit[fn] 
-
-#         param_to_edit = valid_params[para_name]
-
-#     except ValueError:
-#         raise IOError('The provided parameter, {}, is not valid, please provide' \
-#                       ' a valid WRF-Hydro/NWM parameter for the input file {}'.format(para_name, valid_files_to_edit)
-#                       )
-
-#     except KeyError:
-#         raise IOError('The provided parameter file, {}, is not valid, please provide a valid WRF-Hydro/NWM parameter file'.format(valid_files_to_edit))
-
-
-
-
-#     valid_files_to_edit = {'Route_Link.nc', 'GWBUCKPARM.nc', 'LAKEPARM.nc', 'Soil_properties.nc', 'Fulldom_hires.nc'}
-
-#     if fn not in valid_files_to_edit:
-#         raise ValueError("results: file_name to be edited must be one of %r." % valid_files_to_edit)
-
-#     if fn == 'Route_link.nc':
-#         # For now, don't play with 'TopWdth'
-#         valid_para_to_edit = {'ChSlp', 'n', 'nCC', 'TopWdth', 'TopWdthCC', 'BtmWdth'}
-#         if para_name not in valid_para_to_edit:
-#             raise ValueError("results: parameter_name to be edited must be one of %r." % valid_para_to_edit)
-
-
-#     elif fn == 'GWBUCKPARM.nc':
-#         # For now, don't play with 'TopWdth'
-#         valid_para_to_edit = {'Expon', 'Zinit', 'Zmax'}
-#         if para_name not in valid_para_to_edit:
-#             raise ValueError("results: parameter_name to be edited must be one of %r." % valid_para_to_edit)
-
-
-#     elif fn == 'LAKEPARM.nc':
-#         # For now, don't play with 'TopWdth'
-#         valid_para_to_edit = {'OrificeA', 'OrificeC', 'OrificeE', 'WeirC', 'WeirE', 'WeirL'}
-#         if para_name not in valid_para_to_edit:
-#             raise ValueError("results: parameter_name to be edited must be one of %r." % valid_para_to_edit)
-
-
-#     elif fn == 'Soil_properties.nc':
-#         # For now, don't play with 'TopWdth'
-#         valid_para_to_edit = {'mfsno'}
-#         if para_name not in valid_para_to_edit:
-#             raise ValueError("results: parameter_name to be edited must be one of %r." % valid_para_to_edit)
-
-
-#     elif fn == 'Fulldomain_hires.nc':
-#         # For now, don't play with 'TopWdth'
-#         valid_para_to_edit = {'LKSATFAC', 'OVROUGHRTFAC', 'RETDEPRTFAC'}
-#         if para_name not in valid_para_to_edit:
-#             raise ValueError("results: parameter_name to be edited must be one of %r." % valid_para_to_edit)
-
-
-#     ds[para_name][:] *= scale  # Modify the parameter
-#     ds.attrs['Edits_made'] = metadata_string(para_name, '*', scale) # Modify the MetaData
-
-#     # !!!Be advised: should better define the cs_area, currently, it is a xarray dataframe and the variable name is
-#     # not correct...but it works!
-#     if para_name == 'ChSlp':
-#         #  Following 2 equations are from Blackburn et al.
-#         cs_area = cross_section_area_BlckBrn(ds['TopWdth'][:])  # cs_area is not a varaible in NWM dataframe
-#         # but is necessary to be calculated in order to be able to calculate BtmWdth to
-
-#         # Be careful of neg values being root squared: happens when dramatically decrease ChSlp
-#         ds['BtmWdth'][:] = (ds['TopWdth'][:] ** 2 - 4.0 * cs_area * ds['ChSlp'][:]) ** 0.5
-
-#         # ds.attrs['Edits_made'] += ' ** Also, para ' + 'BtmWdth' + ' was changed as dependent para '  # Modify the MetaData
-#         # TODO: Might need to add for dependent parameter case?
-#         ds.attrs['Edits_made'] += metadata_string('BtmWdth', '', '', key='d')
-
-
-#     if para_name == 'BtmWdth':
-#         # Following 2 equations are from Blackburn et al.
-#         # The calculation of updated Channel slope IS NOT consistent
-#         # with current manuscripts, however it is correct despite not being
-#         # documented correctly in Wrf-Hydro documentation.
-#         cs_area = cross_section_area_BlckBrn(ds['TopWdth'][:])
-#         ds['ChSlp'][:] = 1/((ds['TopWdth'][:] ** 2 - ds['BtmWdth'][:] ** 2) / (4.0 * cs_area))
-
-#         # TODO: Might need to add for dependent parameter case?
-#         ds.attrs['Edits_made'] = metadata_string('ChSlp', '', '', key='d')
-
-#     return ds
-
-# def para_editor_evenly_scaler_multi_para(InputFilepath, InputFilename, outputpath, para_names, scales_list):
-#     '''(Input file to be edited, outputpath, list of parameters to be edited, list of scales corresponding to the parameters)'''
-#     # This function scales multiple parameters and the dependent parameters if any.
-
-#     # TODO: For Iman: Raise values error when len(para_names) != len(scales_list)
-#     ds = xr.open_dataset(InputFilepath + '\\' + InputFilename)  # Open the netcdf file
-
-#     ds1 = ds  # Copy the dataset
-
-#     attr = ''
-#     # edit the netcdf files
-#     for i, para_name in enumerate(para_names):
-#         ds1 = para_editor_evenly_scaler(InputFilename, ds1, para_name, scales_list[i])
-#         attr = attr + ds1.attrs['Edits_made']
-
-#     ds1.attrs['Edits_made'] = attr  # Add Global Attribute to track the changes made on parameters
-
-#     # TODO: do sth like this: f = open(os.path.join(reportfile_directory, "PerfMetrReport.csv"), "w")
-#     # Create the outputfile name
-#     for i, para_name in enumerate(para_names):
-#         outputpath += '_p' + str(i) + '_' + para_name + '_sc_' + str(scales_list[i])
-#     outputpath += '.nc'
-
-#     # Create the edited netcdf file
-#     ds1.to_netcdf(outputpath)
-
-#     return ds1, outputpath  # Austing said change to outputpath
-
+# Code below has been kept mainly to preserve the thoughts held within the
+# code. This will be refactored into either functions or implemented within
+# existing functions 
 
 # # TODO: Handle the streamorder problem metadata and also changing mulitiple parameters at the same time
 # def para_editor_streamorder_based_scaler(ds, para_name, streamorder_list, scale_list):
@@ -401,6 +253,7 @@ if __name__ == "__main__":
     from JobScheduler import filehandler
 
     # Test _apply_functions
+    # TODO: Remove and add to test
     route_link = '/Users/austinraney/Box Sync/si/nwm/domains/031601_domain1_nwm_cutout/Route_Link.nc'
     parameters = ['n', 'n']
     operators = ['^', '+']
