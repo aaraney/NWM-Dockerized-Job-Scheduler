749aa1dadf1564868b734657692bbadf4add786a
Austin Raney <aaraney@crimson.ua.edu>
2020-08-20 10:07:07 -0400

    Update list_run.py

--- pocono_test_case/list_run.py
+++ pocono_test_case/list_run.py
@@ -1,9 +1,9 @@
-from os.path import realpath
+from pathlib import Path
 import sys
-sys.path.append('../framework/JobScheduler')
-from scheduler import *
 
-primary_path = realpath('primary')
+import djs
+
+primary_path = Path('primary').resolve()
 altered_domain_files = [
     "Route_Link.nc",
     "Route_Link_1.nc",
@@ -11,8 +11,13 @@ altered_domain_files = [
     "Route_Link_3.nc",
     "Route_Link_4.nc"
 ]
+
+def full_path_helper(file: str) -> Path:
+    return Path(file).resolve()
+
 # Map full file system path name to altered domain files
-altered_domain_files = map(lambda f: realpath(f), altered_domain_files)
+altered_domain_files = [full_path_helper(file) for file in altered_domain_files]
+
 schedule = Scheduler.fromList(primary_path, altered_domain_files)
 schedule.max_jobs = 2
 schedule.mpi_np = 2
